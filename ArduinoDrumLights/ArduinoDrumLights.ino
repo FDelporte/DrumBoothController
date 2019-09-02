0c63ce3ace3b2b71fc8c1f0c983bbe3f109eb9b1
Frank Delporte <f.delporte@televic.com>
2019-09-02 23:54:42 +0200

    Refactored to have one code base for both master and slave boards

---
+++ ArduinoDrumLights/ArduinoDrumLights.ino
@@ -0,0 +1,253 @@
+// https://randomnerdtutorials.com/guide-for-ws2812b-addressable-rgb-led-strip-with-arduino/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <FastLED.h>
+
+// LED settings
+#define MASTER_PIN  10
+#define LED_PIN     5
+#define NUM_LEDS    14
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+#define UPDATES_PER_SECOND 100
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+// Radio settings
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+
+// Color selection inputs
+#define ANALOG_PIN_POT_R 0
+#define ANALOG_PIN_POT_G 1
+#define ANALOG_PIN_POT_B 2
+
+// Selected values
+byte colorR = 0xff;
+byte colorG = 0xff;
+byte colorB = 0xff;
+
+boolean isSlave = true;
+
+void setup() {
+  // Power-up safety delay
+  delay(3000); 
+  
+  // Initialize color leds
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+  
+  currentPalette = RainbowColors_p;
+  currentBlending = LINEARBLEND;
+    
+  // Define the master/slave state
+  pinMode(MASTER_PIN, INPUT_PULLUP);   
+  Serial.print("This board is ");
+  if (digitalRead(MASTER_PIN) == HIGH) {
+    isSlave = false;
+    Serial.println("SLAVE");
+  } else {
+    Serial.println("MASTER");
+  }
+
+  // Start the radio
+  radio.begin();                  //Starting the Wireless communication
+
+  if (isSlave) {
+    radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+    radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+    radio.startListening();              //This sets the module as receiver
+  } else {
+    radio.openWritingPipe(address); //Setting the address where we will send the data
+    radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+    radio.stopListening();          //This sets the module as transmitter
+  }
+}
+
+void loop() {
+  if (isSlave) {
+    receiveValuesFromRadio();
+    updateLeds();  
+    delay(5);
+  } else {
+    readValuesFromInputs();
+    sendValuesOverRadio();
+    updateLeds();    
+    delay(1000);
+  }
+}
+
+void readValuesFromInputs() {
+  // Read the selected color
+  colorR = analogRead(ANALOG_PIN_POT_R) / 4;
+  colorG = analogRead(ANALOG_PIN_POT_G) / 4;
+  colorB = analogRead(ANALOG_PIN_POT_B) / 4;
+  
+  // Read the selected effect
+  // TODO  
+}
+
+void sendValuesOverRadio() {
+  radio.write(&colorR, sizeof(colorR));
+  radio.write(&colorG, sizeof(colorG));
+  radio.write(&colorB, sizeof(colorB));
+}
+
+void receiveValuesFromRadio() {
+  if (radio.available()) {
+    // Looking for the data.
+    radio.read(&colorR, sizeof(colorR));
+    radio.read(&colorG, sizeof(colorG));
+    radio.read(&colorB, sizeof(colorB));
+  }
+}
+
+void updateLeds() {
+    Serial.print("Colors: ");
+    Serial.print(colorR, HEX);
+    Serial.print("/");
+    Serial.print(colorG, HEX);
+    Serial.print("/");
+    Serial.print(colorB, HEX);
+    Serial.println("");
+  
+    ChangePalettePeriodically();
+    
+    static uint8_t startIndex = 0;
+    startIndex = startIndex + 1; /* motion speed */
+    
+    FillLEDsFromPaletteColors( startIndex);
+    
+    FastLED.show();
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
+}
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex) {
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
+
+
+// There are several different palettes of colors demonstrated here.
+//
+// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
+// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
+//
+// Additionally, you can manually define your own color palettes, or you can write
+// code that creates color palettes on the fly.  All are shown here.
+
+void ChangePalettePeriodically() {
+    uint8_t secondHand = (millis() / 1000) % 60;
+    static uint8_t lastSecond = 99;
+    
+    if (lastSecond != secondHand) {
+        lastSecond = secondHand;
+        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
+        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
+        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
+        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
+        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
+        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
+        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
+        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
+        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
+    }
+}
+
+// This function fills the palette with totally random colors.
+void SetupTotallyRandomPalette() {
+    for( int i = 0; i < 16; i++) {
+        currentPalette[i] = CHSV( random8(), 255, random8());
+    }
+}
+
+// This function sets up a palette of black and white stripes,
+// using code.  Since the palette is effectively an array of
+// sixteen CRGB colors, the various fill_* functions can be used
+// to set them up.
+void SetupBlackAndWhiteStripedPalette() {
+    // 'black out' all 16 palette entries...
+    fill_solid( currentPalette, 16, CRGB::Black);
+    // and set every fourth one to white.
+    currentPalette[0] = CRGB::White;
+    currentPalette[4] = CRGB::White;
+    currentPalette[8] = CRGB::White;
+    currentPalette[12] = CRGB::White;
+    
+}
+
+// This function sets up a palette of purple and green stripes.
+void SetupPurpleAndGreenPalette() {
+    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
+    CRGB green  = CHSV( HUE_GREEN, 255, 255);
+    CRGB black  = CRGB::Black;
+    
+    currentPalette = CRGBPalette16(
+                                   green,  green,  black,  black,
+                                   purple, purple, black,  black,
+                                   green,  green,  black,  black,
+                                   purple, purple, black,  black );
+}
+
+// This example shows how to set up a static color palette
+// which is stored in PROGMEM (flash), which is almost always more
+// plentiful than RAM. A static PROGMEM palette like this
+// takes up 64 bytes of flash.
+const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
+{
+    CRGB::Red,
+    CRGB::Gray, // 'white' is too bright compared to red and blue
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Black,
+    CRGB::Black
+};
+
+// Additionl notes on FastLED compact palettes:
+//
+// Normally, in computer graphics, the palette (or "color lookup table")
+// has 256 entries, each containing a specific 24-bit RGB color.  You can then
+// index into the color palette using a simple 8-bit (one byte) value.
+// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
+// is quite possibly "too many" bytes.
+//
+// FastLED does offer traditional 256-element palettes, for setups that
+// can afford the 768-byte cost in RAM.
+//
+// However, FastLED also offers a compact alternative.  FastLED offers
+// palettes that store 16 distinct entries, but can be accessed AS IF
+// they actually have 256 entries; this is accomplished by interpolating
+// between the 16 explicit entries to create fifteen intermediate palette
+// entries between each pair.
+//
+// So for example, if you set the first two explicit entries of a compact 
+// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
+// the first sixteen entries from the virtual palette (of 256), you'd get
+// Green, followed by a smooth gradient from green-to-blue, and then Blue.
--- README.md
+++ README.md
@@ -3,24 +3,31 @@ Arduino project with
 * one as master with control elements
 * multiple wireless slaves with RGB led lights
 
-## Description
+## One Arduino code for multiple Arduino boards
 
-### Master functions
+### One master board
 * 3 pot meters for R/G/B mixing
 * 1 hex switch for effect selection
 * 1 display for visualization of selected color and effect + connected slaves
-* Controls own RGB led strip
 * Transmits color code and effect
+* Controls own RGB led strip
 
-### Slave functions
+### Multiple slave boards
 * 1 hex switch for addressing (to let the master known if alive)
 * Control of RGB led strip according to received color code and effect
 	
+## Components
+
+### LED Strip
+Type WS2812IC 
+
 ### Wireless
 Via NRF24l01, e.g. https://www.dx.com/p/nrf24l01-2-4ghz-enhanced-wireless-modules-black-4-pcs-2057721
 
 ### Dependencies
-Download https://github.com/nRF24/RF24 and add to Arduino Libraries (e.g. in "C:\Program Files (x86)\Arduino\libraries")
+Download and add to Arduino Libraries (e.g. in "C:\Program Files (x86)\Arduino\libraries"):
+* https://github.com/nRF24/RF24
+* https://github.com/FastLED/FastLED/archive/master.zip
 
 ## Inspired by
 * https://create.arduino.cc/projecthub/muhammad-aqib/nrf24l01-interfacing-with-arduino-wireless-communication-0c13d4?ref=tag&ref_id=wireless&offset=10
